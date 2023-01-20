3dad6a3443ce5fc5f72985a50a9a9c01fd320933
shogok62 <115639287+shogok62@users.noreply.github.com>
2023-01-20 10:38:22 +0900

    Add files via upload

---
+++ gs_hw_0120.sql
@@ -0,0 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- ホスト: 127.0.0.1
+-- 生成日時: 2023-01-20 02:37:50
+-- サーバのバージョン： 10.4.27-MariaDB
+-- PHP のバージョン: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- データベース: `gs_hw_0120`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `gs_content_table`
+--
+
+CREATE TABLE `gs_content_table` (
+  `id` int(12) NOT NULL,
+  `title` varchar(64) NOT NULL COMMENT '記事のタイトル',
+  `content` varchar(256) NOT NULL COMMENT '記事の内容',
+  `img` varchar(256) DEFAULT NULL COMMENT '画像のPATH',
+  `date` datetime NOT NULL COMMENT '登録日',
+  `update_time` datetime DEFAULT NULL COMMENT '更新日（NULL許容）'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci COMMENT='記事のテーブル';
+
+--
+-- テーブルのデータのダンプ `gs_content_table`
+--
+
+INSERT INTO `gs_content_table` (`id`, `title`, `content`, `img`, `date`, `update_time`) VALUES
+(40, '晩年マルクス主義者によるマルクス主義', '斎藤幸平', '20230120014125_saito.jpeg', '2023-01-20 09:41:25', NULL),
+(41, 'ほんまもん', '本家本元', '20230120014221_marx.jpg', '2023-01-20 09:42:21', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `gs_user_table`
+--
+
+CREATE TABLE `gs_user_table` (
+  `id` int(12) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `lid` varchar(128) NOT NULL,
+  `lpw` varchar(64) NOT NULL,
+  `kanri_flg` int(1) NOT NULL,
+  `life_flg` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- テーブルのデータのダンプ `gs_user_table`
+--
+
+INSERT INTO `gs_user_table` (`id`, `name`, `lid`, `lpw`, `kanri_flg`, `life_flg`) VALUES
+(1, 'テスト１管理者', 'test1', '$2y$10$amJsJbnjco8Gyz9bytkawOgWcKDu2PC/hf6daAkGYFZ35nmoq1ETu', 1, 0),
+(2, 'テスト2一般', 'test2', '$2y$10$Jrod7Xts/VPa0ThZhT33AedMO7D2rHLA.qqDLqYGvimBqsP5Xliia', 0, 0),
+(3, 'テスト３', 'test3', '$2y$10$hT8NciYWQ3.lO0QLfz1UHOySxqHYFVN3f3p2tTLQKiIgw0kBvO6g.', 0, 0);
+
+--
+-- ダンプしたテーブルのインデックス
+--
+
+--
+-- テーブルのインデックス `gs_content_table`
+--
+ALTER TABLE `gs_content_table`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- テーブルのインデックス `gs_user_table`
+--
+ALTER TABLE `gs_user_table`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- ダンプしたテーブルの AUTO_INCREMENT
+--
+
+--
+-- テーブルの AUTO_INCREMENT `gs_content_table`
+--
+ALTER TABLE `gs_content_table`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+
+--
+-- テーブルの AUTO_INCREMENT `gs_user_table`
+--
+ALTER TABLE `gs_user_table`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
